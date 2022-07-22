98fe8310cc239083fbb6a83c4a0209cde836c6f2
sarafenuchi@gmail.com <sarafenuchi@gmail.com>
2022-07-22 10:45:46 -0300

    Initial commit

---
+++ app/src/main/java/br/com/servicolimpeza/myappservilimp/sobreActivity.java
@@ -0,0 +1,14 @@
+package br.com.servicolimpeza.myappservilimp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre_layout);
+    }
+}
